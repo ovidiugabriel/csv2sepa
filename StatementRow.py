6d4a7fad544434af471c7f6d3403703ad6e7372d
Ovidiu <ovidiugabriel@gmail.com>
2019-04-24 14:13:37 +0300

    Create StatementRow.py

---
+++ StatementRow.py
@@ -0,0 +1,21 @@
+#
+# This is a row of TransferWise account statement
+#
+class StatementRow:
+    transferwise_id       = None
+    date                  = None
+    amount                = None
+    currency              = None
+    description           = None
+    payment_reference     = None
+    running_balance       = None
+    exchange_from         = None
+    exchange_to           = None
+    exchange_rate         = None
+    payer_name            = None
+    payee_name            = None
+    payee_account_number  = None
+    merchant              = None
+    total_fees            = None
+
+# end class StatementRow
