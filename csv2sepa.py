0a31e93d94e2d439dfcdb31788d322e3532547c5
Ovidiu <ovidiugabriel@gmail.com>
2019-04-24 14:36:21 +0300

    Moved StatementRow to StatementRow.py

--- csv2sepa.py
+++ csv2sepa.py
@@ -30,24 +30,6 @@ config = {
     }
 }
 
-# This is a row of TransferWise account statement
-class StatementRow:
-    transferwise_id       = None
-    date                  = None
-    amount                = None
-    currency              = None
-    description           = None
-    payment_reference     = None
-    running_balance       = None
-    exchange_from         = None
-    exchange_to           = None
-    exchange_rate         = None
-    payer_name            = None
-    payee_name            = None
-    payee_account_number  = None
-    merchant              = None
-    total_fees            = None
-
 # Parses a row in the CSV file and returns a Transfer
 def parse_row(row):
     i = 0;
